b79e584e018ecaf45949380db4c2512937056441
“John <little78926@gmail.com>
2016-01-20 00:41:02 -0800

    agian

--- db/schema.rb
+++ db/schema.rb
@@ -230,22 +230,22 @@ ActiveRecord::Schema.define(version: 20160115055359) do
 
   create_table "users", force: true do |t|
     t.string   "name"
-    t.string   "email",                  default: "", null: false
+    t.string   "email",                  default: "",    null: false
     t.boolean  "check_teacher"
     t.boolean  "check_member"
     t.boolean  "check_staff"
     t.string   "position"
-    t.binary   "encrypted_password",     default: "", null: false
+    t.binary   "encrypted_password",     default: "x''", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
     t.string   "firstname"
     t.string   "lastname"
     t.string   "middlename"
