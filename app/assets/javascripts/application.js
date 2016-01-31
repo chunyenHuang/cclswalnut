475fb4b01e840e73939c2a760e3e70099d2542ce
“John <little78926@gmail.com>
2016-01-30 22:46:25 -0800

    new one

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -29,18 +29,7 @@
 //= require dropdown
 //= require turbolinks
 //= require_tree .
-jQuery(function($){
-   $("#date").mask("99/99/9999",{placeholder:"mm/dd/yyyy"});
-   $("#phone").mask("(999) 999-9999");
-   $("#phone2").mask("(999) 999-9999");
-   $("#phone3").mask("(999) 999-9999");
-   $("#phone4").mask("(999) 999-9999");
-   $("#phone5").mask("(999) 999-9999");
-   $("#phone6").mask("(999) 999-9999");
-   $("#tin").mask("99-9999999");
-   $("#ssn").mask("999-99-9999");
-   $("#money").mask("999,999");
-});
+
 
 $('input[type=file]').bootstrapFileInput();
 $('.file-inputs').bootstrapFileInput();
