4aa263d98749374167310643ed6bb10f6ca44fbd
“John <little78926@gmail.com>
2016-01-31 03:24:52 -0800

    logout problem

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module FactoryStatusApp
     config.time_zone = 'Pacific Time (US & Canada)'
     #config.wkhtmltopdf = ('which wkhtmltopdf').chomp
     config.middleware.use "PDFKit::Middleware", :print_media_type => true
-    # config.serve_static_assets = true
+    config.serve_static_assets = true
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
