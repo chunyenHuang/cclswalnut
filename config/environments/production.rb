6bde3241784f0f27b721ca174e8ab0c7961b878f
“John <little78926@gmail.com>
2016-02-13 14:26:06 -0800

    well

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,7 +56,8 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.action_controller.asset_host = "//s3.amazonaws.com/school-system-asset"
+  # config.action_controller.asset_host = "//s3.amazonaws.com/school-system-asset"
+  
   # config.action_controller.asset_host = "//s3.amazonaws.com/#{ENV['FOG_DIRECTORY']}"
   #  config.assets.prefix = "/school-system-asset"
 
