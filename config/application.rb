424a367a7a6a8ed20ad9d9a07c106bb98102f9cb
“John <little78926@gmail.com>
2016-02-13 16:57:01 -0800

    a

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,9 @@ module FactoryStatusApp
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.assets.enabled = true
+   
+    # original= true
+    config.assets.enabled = false
 
     #Needs to be false on Heroku
     config.assets.initialize_on_precompile = false
